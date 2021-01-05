2cad7c80383d481ba6853adae6c1ac29cd966482
nitlara <nitlara@gmail.com>
2021-01-05 14:15:36 +0000

    4 zeros are visible

---
+++ src/js/component/simple-individual-use-component.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+
+const element = <FontAwesomeIcon icon={faClock} />;
+
+ReactDOM.render(element, document.body);
+
+//<i class="fas fa-clock"></i>
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //import react into the bundle
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 //include bootstrap npm library into the bundle
@@ -8,8 +8,21 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+// your own components
+// previous component in boilerplate import { Home } from "./component/home.js";
+function SimpleCounter(props) {
+	return (
+		<div className="Counter">
+			<div className="calendar">
+				<i className="far-fa-clock" />
+			</div>
+			<div className="four">0</div>
+			<div className="tree">0</div>
+			<div className="two">0</div>
+			<div className="one">0</div>
+		</div>
+	);
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
