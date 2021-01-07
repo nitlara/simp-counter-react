8adee9e4b0cbb268638805dd8de071e14f711de9
nitlara <nitlara@gmail.com>
2021-01-07 14:59:01 +0000

    the counter works and is displayed on the web

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -16,13 +17,37 @@ function SimpleCounter(props) {
 			<div className="calendar">
 				<i className="far-fa-clock" />
 			</div>
-			<div className="four">0</div>
-			<div className="tree">0</div>
-			<div className="two">0</div>
-			<div className="one">0</div>
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="tree">{props.digitThree % 10}</div>
+			<div className="two">{props.digitTwo % 10}</div>
+			<div className="one">{props.digitOne % 10}</div>
 		</div>
 	);
 }
 
-//render your react application
-ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
+SimpleCounter.propTypes = {
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
+};
+
+let counter = 0;
+setInterval(function() {
+	const four = Math.floor(counter / 10000);
+	const three = Math.floor(counter / 1000);
+	const two = Math.floor(counter / 100);
+	const one = Math.floor(counter / 10);
+	counter++;
+
+	//render your react application
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 100);
