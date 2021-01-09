682146aa3d1a11ff7cdc64ed25f2c3ebe54b0cf6
nitlara <nitlara@gmail.com>
2021-01-09 18:27:51 +0000

    added start and stop without actives and inactives

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //import react into the bundle
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 // get our fontawesome imports
@@ -25,10 +25,10 @@ function SimpleCounter(props) {
 				<div className="one">{props.digitOne % 10}</div>
 			</div>
 			<div className="GroupButtons pl-4 pb-3">
-				<div type="button" className="btn btn-dark m-2 " />
-				Start
-                </div>
-				<div type="button" className="btn btn-dark m-2">
+				<div type="button" className="btn btn-dark m-2" onClick={start}>
+					Start
+				</div>
+				<div type="button" className="btn btn-dark m-2" onClick={stop}>
 					Stop
 				</div>
 				<div type="button" className="btn btn-dark m-2" onClick={reset}>
@@ -47,26 +47,35 @@ SimpleCounter.propTypes = {
 };
 
 let counter = 0;
+let interval = null;
 
 function reset() {
 	counter = 0;
 }
 
-setInterval(function() {
-	const four = Math.floor(counter / 10000);
-	const three = Math.floor(counter / 1000);
-	const two = Math.floor(counter / 100);
-	const one = Math.floor(counter / 10);
-	counter++;
+function start() {
+	interval = setInterval(function() {
+		const four = Math.floor(counter / 10000);
+		const three = Math.floor(counter / 1000);
+		const two = Math.floor(counter / 100);
+		const one = Math.floor(counter / 10);
+		counter++;
 
-	//render your react application
-	ReactDOM.render(
-		<SimpleCounter
-			digitOne={one}
-			digitTwo={two}
-			digitThree={three}
-			digitFour={four}
-		/>,
-		document.querySelector("#app")
-	);
-}, 100);
+		//render your react application
+		ReactDOM.render(
+			<SimpleCounter
+				digitOne={one}
+				digitTwo={two}
+				digitThree={three}
+				digitFour={four}
+			/>,
+			document.querySelector("#app")
+		);
+	}, 100);
+}
+
+function stop() {
+	clearInterval(interval);
+}
+
+start();
